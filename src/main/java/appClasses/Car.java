48bc1147a00329d499acf9945f17f6c0acea2abb
Aliaksandr Medziuk <aleksandrmeduyk@gmail.com>
2019-09-08 15:27:50 +0300

    Add Car class

---
+++ src/main/java/appClasses/Car.java
@@ -0,0 +1,55 @@
+package appClasses;
+
+public class Car {
+    private String make;
+    private String model;
+    private String year;
+    private String engine = "";
+    private String transmission = "";
+
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public boolean areEngineAndTransSaved() {
+        return !(engine.equals("") && transmission.equals(""));
+    }
+
+}
--- src/test/java/CarsComTest.java
+++ src/test/java/CarsComTest.java
@@ -1,3 +1,4 @@
+import appClasses.Car;
 import framework.browser.Browser;
 import framework.utils.LoggerUtil;
 import framework.utils.PropertyManager;
@@ -44,19 +45,21 @@ public class CarsComTest {
 
         ResearchPage researchPage = new ResearchPage();
         assertTrue(researchPage.getSelectCarMenu().isDisplayed(), "This is not the research page");
-        String firstCarMake = researchPage.getSelectCarMenu().chooseRandomOption(make);
-        String firstCarModel = researchPage.getSelectCarMenu().chooseRandomOption(model);
-        String firstCarYear = researchPage.getSelectCarMenu().chooseRandomOption(year);
+        Car firstCar = new Car();
+        firstCar.setMake(researchPage.getSelectCarMenu().chooseRandomOption(make));
+        firstCar.setModel(researchPage.getSelectCarMenu().chooseRandomOption(model));
+        firstCar.setYear(researchPage.getSelectCarMenu().chooseRandomOption(year));
         researchPage.getSelectCarMenu().searchCar();
 
         CarPage carPage = new CarPage();
-        assertTrue(carPage.isChosenCar(firstCarMake, firstCarModel, firstCarYear), "This is not the page for first chosen car");
+        assertTrue(carPage.isChosenCar(firstCar.getMake(), firstCar.getModel(), firstCar.getYear()), "This is not the page for first chosen car");
         carPage.clickOnCompareTrims();
 
         CompareTrimsPage compareTrimsPage = new CompareTrimsPage();
-        assertTrue(compareTrimsPage.isCompareTrimsPage(firstCarMake, firstCarModel, firstCarYear), "This is note hte compare trims page for the first car");
-        String firstCarEngine = compareTrimsPage.getCharacteristic(engine);
-        String firstCarTrans = compareTrimsPage.getCharacteristic(transmission.substring(0, 5));
+        assertTrue(compareTrimsPage.isCompareTrimsPage(firstCar.getMake(), firstCar.getModel(), firstCar.getYear()), "This is not the compare trims page for the first car");
+        firstCar.setEngine(compareTrimsPage.getCharacteristic(engine));
+        firstCar.setTransmission(compareTrimsPage.getCharacteristic(transmission.substring(0, 5)));
+        assertTrue(firstCar.areEngineAndTransSaved(), "Engine and trans did not saved for the first car");
         compareTrimsPage.getNavigationMenu().navigateToMainPage();
 
         HomePage secondInstance = new HomePage();
@@ -65,19 +68,21 @@ public class CarsComTest {
 
         ResearchPage secondResearchPage = new ResearchPage();
         assertTrue(secondResearchPage.getSelectCarMenu().isDisplayed(), "This is not the research page");
-        String secondCarMake = secondResearchPage.getSelectCarMenu().chooseRandomOption(make);
-        String secondCarModel = secondResearchPage.getSelectCarMenu().chooseRandomOption(model);
-        String secondCarYear = secondResearchPage.getSelectCarMenu().chooseRandomOption(year);
+        Car secondCar = new Car();
+        secondCar.setMake(secondResearchPage.getSelectCarMenu().chooseRandomOption(make));
+        secondCar.setModel(secondResearchPage.getSelectCarMenu().chooseRandomOption(model));
+        secondCar.setYear(secondResearchPage.getSelectCarMenu().chooseRandomOption(year));
         secondResearchPage.getSelectCarMenu().searchCar();
 
         CarPage secondCarPage = new CarPage();
-        assertTrue(secondCarPage.isChosenCar(secondCarMake, secondCarModel, secondCarYear), "This is not the page for second chosen car");
+        assertTrue(secondCarPage.isChosenCar(secondCar.getMake(), secondCar.getModel(), secondCar.getYear()), "This is not the page for second chosen car");
         secondCarPage.clickOnCompareTrims();
 
         CompareTrimsPage secondCompareTrimsPage = new CompareTrimsPage();
-        assertTrue(secondCompareTrimsPage.isCompareTrimsPage(secondCarMake, secondCarModel, secondCarYear), "This is note hte compare trims page for the second car");
-        String secondCarEngine = secondCompareTrimsPage.getCharacteristic(engine);
-        String secondCarTrans = secondCompareTrimsPage.getCharacteristic(transmission.substring(0, 5));
+        assertTrue(secondCompareTrimsPage.isCompareTrimsPage(secondCar.getMake(), secondCar.getModel(), secondCar.getYear()), "This is note hte compare trims page for the second car");
+        secondCar.setEngine(secondCompareTrimsPage.getCharacteristic(engine));
+        secondCar.setTransmission(secondCompareTrimsPage.getCharacteristic(transmission.substring(0, 5)));
+        assertTrue(secondCar.areEngineAndTransSaved(), "Engine and trans did not saved for the second car");
         secondCompareTrimsPage.getNavigationMenu().navigateToResearch();
 
         ResearchPage thirdResearchPage = new ResearchPage();
@@ -86,26 +91,26 @@ public class CarsComTest {
 
         CompareSideBySidePage compareSideBySidePage = new CompareSideBySidePage();
         assertTrue(compareSideBySidePage.isCompareSBSPage(), "This in not the compare side by side page");
-        compareSideBySidePage.getSideBySideCompareMenu().chooseOption(make, firstCarMake);
-        compareSideBySidePage.getSideBySideCompareMenu().chooseOption(model, firstCarModel);
-        compareSideBySidePage.getSideBySideCompareMenu().chooseOption(year, firstCarYear);
+        compareSideBySidePage.getSideBySideCompareMenu().chooseOption(make, firstCar.getMake());
+        compareSideBySidePage.getSideBySideCompareMenu().chooseOption(model, firstCar.getModel());
+        compareSideBySidePage.getSideBySideCompareMenu().chooseOption(year, firstCar.getYear());
         compareSideBySidePage.getSideBySideCompareMenu().submitCar();
 
         ModelComparePage modelComparePage = new ModelComparePage();
-        assertTrue(modelComparePage.isDisplayedForCar(firstCarMake, firstCarModel, firstCarYear), "This in not the model compare page for the first car");
+        assertTrue(modelComparePage.isDisplayedForCar(firstCar.getMake(), firstCar.getModel(), firstCar.getYear()), "This in not the model compare page for the first car");
         modelComparePage.clickOnAddAnotherCar();
-        modelComparePage.getSideBySideCompareMenu().chooseOption(make, secondCarMake);
-        modelComparePage.getSideBySideCompareMenu().chooseOption(model, secondCarModel);
-        modelComparePage.getSideBySideCompareMenu().chooseOption(year, secondCarYear);
+        modelComparePage.getSideBySideCompareMenu().chooseOption(make, secondCar.getMake());
+        modelComparePage.getSideBySideCompareMenu().chooseOption(model, secondCar.getModel());
+        modelComparePage.getSideBySideCompareMenu().chooseOption(year, secondCar.getYear());
         modelComparePage.getSideBySideCompareMenu().addAnotherCar();
-        assertTrue(modelComparePage.isDisplayedForCar(secondCarMake, secondCarModel, secondCarYear), "This in not the model compare page for the second car");
+        assertTrue(modelComparePage.isDisplayedForCar(secondCar.getMake(), secondCar.getModel(), secondCar.getYear()), "This in not the model compare page for the second car");
         String readEngineForFirstCar = modelComparePage.getValueForCar(engine, 1);
         String readEngineForSecondCar = modelComparePage.getValueForCar(engine, 2);
         String readTransmissionForFirstCar = modelComparePage.getValueForCar(transmission, 1);
         String readTransmissionForSecondCar = modelComparePage.getValueForCar(transmission, 2);
-        assertTrue(readEngineForFirstCar.toLowerCase().contains(firstCarEngine.toLowerCase()), "Engines for the first car are not the same");
-        assertTrue(readEngineForSecondCar.toLowerCase().contains(secondCarEngine.toLowerCase()), "Engines for the second car are not the same");
-        assertTrue(readTransmissionForFirstCar.toLowerCase().contains(firstCarTrans.toLowerCase()), "Transmissions for the first car are not the same");
-        assertTrue(readTransmissionForSecondCar.toLowerCase().contains(secondCarTrans.toLowerCase()), "Transmissions for the second car are not the same");
+        assertTrue(readEngineForFirstCar.toLowerCase().contains(firstCar.getEngine().toLowerCase()), "Engines for the first car are not the same");
+        assertTrue(readEngineForSecondCar.toLowerCase().contains(secondCar.getEngine().toLowerCase()), "Engines for the second car are not the same");
+        assertTrue(readTransmissionForFirstCar.toLowerCase().contains(firstCar.getTransmission().toLowerCase()), "Transmissions for the first car are not the same");
+        assertTrue(readTransmissionForSecondCar.toLowerCase().contains(secondCar.getTransmission().toLowerCase()), "Transmissions for the second car are not the same");
     }
 }
